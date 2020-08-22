917b91042e8bf4be7884db3efdca45374cd91e44
Alfredo Patricius Tarigan <62396803+alfredoptarigan@users.noreply.github.com>
2020-08-22 23:55:00 +0700

    Adding Login System

---
+++ AplikasiPenjualan/FormLogin.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Data.Odbc
+Public Class FormLogin
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+    End Sub
+    Sub Terbuka()
+        FormMenuUtama.LoginToolStripMenuItem.Enabled = False
+        FormMenuUtama.LogoutToolStripMenuItem.Enabled = True
+        FormMenuUtama.MasterToolStripMenuItem.Enabled = True
+        FormMenuUtama.TransaksiToolStripMenuItem.Enabled = True
+        FormMenuUtama.LaporanToolStripMenuItem.Enabled = True
+        FormMenuUtama.UtilitiesToolStripMenuItem.Enabled = True
+    End Sub
+
+    Sub KondisiAwal()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("Kode Admin dan Password tidak boleh kosong")
+
+        Else
+            Call Koneksi()
+            Cmd = New OdbcCommand("Select * From tbl_admin where kodeadmin ='" & TextBox1.Text & "' and password = '" & TextBox2.Text & "'", Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            If Rd.HasRows Then
+                Me.Close()
+                Call Terbuka()
+            Else
+                MsgBox("Kode Admin atau Password salah...")
+            End If
+        End If
+    End Sub
+
+    Private Sub FormLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call KondisiAwal()
+    End Sub
+End Class
---
+++ AplikasiPenjualan/FormMenuUtama.vb
@@ -0,0 +1,22 @@
+﻿Public Class FormMenuUtama
+    Sub Terkunci()
+        LoginToolStripMenuItem.Enabled = True
+        LogoutToolStripMenuItem.Enabled = False
+        MasterToolStripMenuItem.Enabled = False
+        TransaksiToolStripMenuItem.Enabled = False
+        LaporanToolStripMenuItem.Enabled = False
+        UtilitiesToolStripMenuItem.Enabled = False
+    End Sub
+
+    Private Sub FormMenuUtama_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call Terkunci()
+    End Sub
+
+    Private Sub LoginToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoginToolStripMenuItem.Click
+        FormLogin.ShowDialog()
+    End Sub
+
+    Private Sub LogoutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LogoutToolStripMenuItem.Click
+        Call Terkunci()
+    End Sub
+End Class
